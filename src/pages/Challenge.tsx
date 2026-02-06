77bdac453512b5434d8ffe074d044bdd9d2a0202
Diego Lajusticia <diegolajusticiasujas@icloud.com>
2026-02-06 22:49:21 +0100

    Refactor Challenge

--- src/pages/Challenge.tsx
+++ src/pages/Challenge.tsx
@@ -2,19 +2,26 @@ import { useParams } from 'react-router-dom';
 import { challenges } from '../data/challenges';
 
 export default function Challenge() {
+    
     const { id } = useParams();
     const challenge = challenges.find(c => c.id === Number(id));
+
+    if (!challenge) {
+        return (
+            <div className="min-h-screen bg-background flex items-center justify-center">
+                <h2 className="text-2xl font-bold">Challenge Not Found</h2>
+            </div>
+        )
+    }
+
     return (
-        <div className="">
-            {challenge ? (  
-                <div className="min-h-screen bg-background flex items-center justify-center">
-                    <h2 className="text-2xl font-bold">Challenge Details Page for ID: {challenge.id}</h2>
-                </div>
-            ) : (
-                <div className="min-h-screen bg-background flex items-center justify-center">
-                    <h2 className="text-2xl font-bold">Challenge Not Found</h2>
-                </div>
-            )}
+        <div>
+            <div className="min-h-screen bg-background flex items-center justify-center">
+                <h2 className="text-2xl font-bold">Challenge Details Page for ID: {challenge.id}</h2>
+            </div>
+            <div className="min-h-screen bg-background flex items-center justify-center">
+                <h2 className="text-2xl font-bold">Challenge Not Found</h2>
+            </div>
         </div>
     );
 }
